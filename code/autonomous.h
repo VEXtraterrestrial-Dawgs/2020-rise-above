12e7d0668f8549595a102288e203f8cb3c6e39a1
Tavas Bharadwaj <71606922+tavasbh@users.noreply.github.com>
2020-12-21 13:36:19 -0800

    Restructured code, filled out PIDControl logic and made PIDControl function universal.

--- code/autonomous.h
+++ code/autonomous.h
@@ -5,136 +5,17 @@ const int DRIVE_GEAR_RATIO = 5;
 const int ARRAY_LENGTH = 5;
 const int THRESHOLD = 1;
 const int LEFT_RIGHT_THRESHOLD = 1;
-const int DRIVETRAIN_WIDTH = 50;
+const int DRIVETRAIN_WIDTH = 132;
 const int SHORT_INTERVAL = 10;
 const int LONG_INTERVAL = 75;
+const int ENCODER_UNITS_PER_ROTATION = 960;
 
 typedef struct {
-	int targetPosition;
-	int targetHeading;
-	int lastEncodersLeft[ARRAY_LENGTH];
-	int lastEncodersRight[ARRAY_LENGTH];
-	int lastGyros[ARRAY_LENGTH];
+	int lastError;
 	int lastTime;
-	int writeSlot;
-	int writtenSlots;
+	float Kp;
+	float Ki;
+	float Kd;
+	float Ka; // Integral Aging Factor
+	float integral;
 } PidObject;
-
-bool isCancelled()
-{
-	// TODO: Read the touch LED and return true if it's been touched.
-	return false;
-}
-
-// Return value says if motion is completed
-bool PIDControl(PidObject* pid) {
-	// Retrieve Values
-	float currentEncoderLeft = getMotorEncoder(leftWheels);
-	float currentEncoderRight = getMotorEncoder(rightWheels);
-
-	// See if we are at our target position yet
-	if (currentEncoderLeft >= (pid->targetPosition - THRESHOLD) &&
-		  currentEncoderLeft <= (pid->targetPosition + THRESHOLD) &&
-		  currentEncoderRight >= (pid->targetPosition - THRESHOLD) &&
-		  currentEncoderRight <= (pid->targetPosition + THRESHOLD))
-	{
-		return true;
-  }
-
-  // TODO: If we are not at our target position, we need to make corrections to our speed so we reach our target
-
-  // If the left and right sides of the drivetrain have different encoder counts, we are not moving in a
-  // straight line so we need to fix that
-  if(fabs(currentEncoderLeft - currentEncoderRight) < LEFT_RIGHT_THRESHOLD) {
-  	// TODO: Some sort of pid logic that adjusts the left and right speeds - look at previous left/right differences
-  }
-
-  int distanceFromTarget = pid->targetPosition - currentEncoderLeft;
-  // TODO: add some basic logic that adjusts the left and right speeds based on distanceFromTarget
-  // If we are driving motors inside PIDControl, we can have an internal variable for left and right speeds,
-  // If driving motors from driveRobot, store left and right speeds in pid object
-
-  // Update values of the pid object and return true
-  pid->lastEncodersLeft[pid->writeSlot] = currentEncoderLeft;
-  pid->lastEncodersRight[pid->writeSlot] = currentEncoderRight;
- 	if(pid->writtenSlots < ARRAY_LENGTH) { pid->writtenSlots++; }
- 	pid->writeSlot = (pid->writeSlot + 1) % ARRAY_LENGTH;
- 	pid->lastTime = nPgmTime;
-
-	return true;
-}
-
-bool driveRobot(int distanceInMM)
-{
-	PidObject pid;
-	bool isComplete = false;
-
-	resetMotorEncoder(leftWheels);
-	resetMotorEncoder(rightWheels);
-	resetGyro(gyro);
-
-	// Given how far we want to go in millimeters, find how far we want to go in encoder units
-	float encoderTarget = distanceInMM / (WHEEL_CIRCUMFERENCE * DRIVE_GEAR_RATIO);
-
-	// Initialize other values of pid object
-	pid.targetPosition = encoderTarget;
-	pid.writeSlot = 0;
-	pid.writtenSlots = 0;
-
-	while (!isCancelled())
-	{
-		isComplete = PIDControl(&pid);
-		if (isComplete)
-		{
-			isComplete = true;
-			break;
-		}
-		sleep(75);
-	}
-
-	setMotorSpeed(leftWheels, 0);
-	setMotorSpeed(rightWheels, 0);
-	return isComplete;
-}
-
-bool turnRobot(int angle) {
-	float angleRadians = degreesToRadians(angle);
-	float distanceMM = (DRIVETRAIN_WIDTH / 2) * angleRadians;
-	float distanceEncoders = distanceMM / (WHEEL_CIRCUMFERENCE * DRIVE_GEAR_RATIO);
-	bool isComplete = false;
-
-	resetMotorEncoder(leftWheels);
-	resetMotorEncoder(rightWheels);
-	resetGyro(gyro);
-
-	while (!isCancelled())
-	{
-		// Check if complete
-		if (false)
-		{
-			isComplete = true;
-			break;
-		}
-		sleep(SHORT_INTERVAL);
-	}
-
-	setMotorSpeed(leftWheels, 0);
-	setMotorSpeed(rightWheels, 0);
-	return isComplete;
-}
-
-bool moveHDrive(int distance) {
-	return true;
-}
-
-bool moveTopArm(int height) {
-	return true;
-}
-
-bool moveBottomArm(int height) {
-	return true;
-}
-
-bool moveBothArms(int height) {
-	return true;
-}
---
+++ code/autonomousfn.c
@@ -0,0 +1,140 @@
+#pragma systemFile
+#include "autonomous.h"
+
+void calibrateGyro()
+{
+	startGyroCalibration(gyro, gyroCalibrateSamples512);
+
+	// delay so calibrate flag can be set internally to the gyro
+	sleep(100);
+
+	eraseDisplay();
+
+	// wait for calibration to finish or 2 seconds, whichever is longer
+	for (int i = 0; getGyroCalibrationFlag(gyro) || (i < 20); i++)
+	{
+		displayTextLine(1, "Calibrating... %02d", i);
+		sleep(100);
+	}
+	displayTextLine(1, "Calibrated!");
+}
+
+bool isCancelled()
+{
+	// TODO: Read the touch LED and return true if it's been touched.
+	return false;
+}
+
+int PIDControl(PidObject* pid, int target, int current) {
+	int error = target - current;
+	int now = nPgmTime;
+	int elapsed = now - pid->lastTime;
+	float derivative = ( error - pid->lastError ) / elapsed;
+
+	pid->integral *= pid->Ka;
+	pid->integral += (error + pid->lastError) * elapsed;
+
+	pid->lastError = error;
+	pid->lastTime = now;
+
+	// Returns a motor speed
+	return round( (error * pid->Kp) + (pid->integral * pid->Ki) + (derivative * pid->Kd) );
+}
+
+bool driveRobot(int distanceInMM)
+{
+	PidObject controllerLeft;
+	PidObject controllerRight;
+	bool isComplete = false;
+
+	resetMotorEncoder(leftWheels);
+	resetMotorEncoder(rightWheels);
+	resetGyro(gyro);
+
+	// Initialize PID controller coefficients to placeholder values
+	controllerLeft.Kp = 1;
+	controllerLeft.Ki = 0;
+	controllerLeft.Kd = 0;
+	controllerLeft.Ka = 0;
+
+	controllerRight.Kp = 1;
+	controllerRight.Ki = 0;
+	controllerRight.Kd = 0;
+	controllerRight.Ka = 0;
+
+	// Given how far we want to go in millimeters, find how far we want to go in encoder units
+	int encoderTarget = (distanceInMM / (WHEEL_CIRCUMFERENCE * DRIVE_GEAR_RATIO)) * ENCODER_UNITS_PER_ROTATION;
+
+	while (!isCancelled())
+	{
+		isComplete = (PIDControl(&controllerLeft, encoderTarget, getMotorEncoder(leftWheels)) == 0);
+		setMotorSpeed(leftWheels, PIDControl(&controllerLeft, encoderTarget, getMotorEncoder(leftWheels)));
+		setMotorSpeed(rightWheels, PIDControl(&controllerRight, getMotorEncoder(leftWheels), getMotorEncoder(rightWheels)));
+
+		if (isComplete)
+		{
+			isComplete = true;
+			break;
+		}
+		sleep(75);
+	}
+
+	setMotorSpeed(leftWheels, 0);
+	setMotorSpeed(rightWheels, 0);
+	return isComplete;
+}
+
+bool turnRobot(int angle) {
+	float angleRadians = degreesToRadians(angle);
+	float distanceMM = (DRIVETRAIN_WIDTH / 2) * angleRadians;
+	float distanceEncoders = (distanceMM / (WHEEL_CIRCUMFERENCE * DRIVE_GEAR_RATIO))*ENCODER_UNITS_PER_ROTATION;
+	bool isComplete = false;
+
+	resetMotorEncoder(leftWheels);
+	resetMotorEncoder(rightWheels);
+	resetGyro(gyro);
+
+	while (!isCancelled())
+	{
+		// Retrieve Sensor Values
+		int encoderLeft = round(getMotorEncoder(leftWheels));
+		int encoderRight = round(getMotorEncoder(rightWheels));
+		float gyroDegrees = getGyroDegreesFloat(gyro);
+
+		// Correct Sensor Values
+		float heading = gyroDegrees;
+
+		// Calculate Motor Speeds
+
+		// Check if complete
+		if (false)
+		{
+			isComplete = true;
+			break;
+		}
+
+		// Set Motor Speeds
+
+		sleep(SHORT_INTERVAL);
+	}
+
+	setMotorSpeed(leftWheels, 0);
+	setMotorSpeed(rightWheels, 0);
+	return isComplete;
+}
+
+bool moveHDrive(int distance) {
+	return true;
+}
+
+bool moveTopArm(int height) {
+	return true;
+}
+
+bool moveBottomArm(int height) {
+	return true;
+}
+
+bool moveBothArms(int height) {
+	return true;
+}
